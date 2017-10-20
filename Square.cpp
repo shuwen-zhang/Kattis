4bf82b9384b860e8f2f34002097f78cd91986b94
shuwen-zhang <shuwen.zh22@gmail.com>
2017-10-19 23:33:28 -0500

    Create Square.cpp

---
+++ Square.cpp
@@ -0,0 +1,27 @@
+// Exponentiation by Squaring
+// https://uchicago.kattis.com/problems/uchicago.mpcs.square
+
+#include <iostream>
+using namespace std;
+
+int exp(int x, int n);
+
+int main() {
+	int x, n;
+	cin >> x >> n;
+	cout << exp(x, n) << endl;;
+	return 0;
+}
+
+int exp(int x, int n) {
+
+	if (n == 0)
+		return 1;
+	else if (n == 1)
+		return x;
+	else if (n % 2 == 0)	// n is even
+		return exp(x*x, n/2);
+	else
+		return exp(x*x, (n-1)/2) * x;
+
+}
